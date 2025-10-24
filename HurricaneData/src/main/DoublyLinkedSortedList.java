2bea172226f4a4ef823ecf017d5cff99997cae04
Cameron's PC <Cameron's PC@192.168.0.64>
2025-10-24 09:28:51 -0600

    Basic Doubly Linked List class structure, unfilled in

---
+++ HurricaneData/src/main/DoublyLinkedSortedList.java
@@ -0,0 +1,72 @@
+package main;
+
+public class DoublyLinkedSortedList implements DoublyLinkedSortedListInterface 
+{
+
+	@Override
+	public HurricaneData getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setNext(DoublyLinkedSortedList next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DoublyLinkedSortedList getNext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setPrevious(DoublyLinkedSortedList previous) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DoublyLinkedSortedList getPrevious() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DoublyLinkedSortedList getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DoublyLinkedSortedList getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DoublyLinkedSortedList remove(HurricaneData toRemove) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(HurricaneData newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ HurricaneData/src/main/Node.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Node 
+{
+	private HurricaneData dataLine = null;
+	private Node next = null;
+	private Node previous = null;
+	
+	
+}
